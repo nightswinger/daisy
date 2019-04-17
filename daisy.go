eac32f70a34cc72225718bdf5974ec4fa358ca8b
Yuichiro Toriyama <yuichiro.toriyama@cach-inc.com>
2019-04-17 13:11:56 +0900

    add some functions

--- daisy.go
+++ daisy.go
@@ -10,6 +10,34 @@ func New(t time.Time) *Daisy {
 	return &Daisy{t}
 }
 
+func Now() *Daisy {
+	return New(time.Now()).BeginningOfDay()
+}
+
+func (daisy *Daisy) Tommorrow() *Daisy {
+	return New(daisy.Time.AddDate(0, 0, 1)).BeginningOfDay()
+}
+
+func (daisy *Daisy) Yesterday() *Daisy {
+	return New(daisy.Time.AddDate(0, 0, -1)).BeginningOfDay()
+}
+
+func (daisy *Daisy) PrevYear() *Daisy {
+	return New(daisy.Time.AddDate(-1, 0, 0)).BeginningOfDay()
+}
+
+func (daisy *Daisy) NextYear() *Daisy {
+	return New(daisy.Time.AddDate(1, 0, 0)).BeginningOfDay()
+}
+
+func (daisy *Daisy) PrevMonth() *Daisy {
+	return New(daisy.Time.AddDate(0, -1, 0)).BeginningOfDay()
+}
+
+func (daisy *Daisy) NextMonth() *Daisy {
+	return New(daisy.Time.AddDate(0, 1, 0)).BeginningOfDay()
+}
+
 func (daisy *Daisy) BeginningOfDay() *Daisy {
 	y, m, d := daisy.Date()
 	return New(time.Date(y, m, d, 0, 0, 0, 0, daisy.Time.Location()))
